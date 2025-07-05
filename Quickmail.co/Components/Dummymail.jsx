3826be98516e19e161a63769d8474ae2afe57558
Atharva Prakash Sonar <atharvasonar654@gmail.com>
2025-07-05 23:48:33 +0530

    minor changes yet skeleton is built

--- Quickmail.co/Components/Dummymail.jsx
+++ Quickmail.co/Components/Dummymail.jsx
@@ -28,27 +28,35 @@ function Dummymail() {
         <div className="h-screen w-full bg-gray-200 p-4 ">
             {/* <h2>Dummy Mails </h2> */}
             
-            <div className='flex p-2 bg-white w-full h-25 rounded-2xl m-2'>
+            <div className='flex p-2 bg-white w-full h-20 rounded-2xl m-2'>
                 <h1><b>mail 1</b></h1> <br />
                 <h2>this is the mail</h2>
             </div>
-             <div className='flex p-2 m-2 bg-white w-full h-25 rounded-2xl'>
+             <div className='flex p-2 m-2 bg-white w-full h-20 rounded-2xl'>
                 <h1><b>mail 2</b></h1> <br />
                 <h2>this is the mail</h2>
             </div>
-             <div className='flex p-2 m-2 bg-white w-full h-25 rounded-2xl'>
-                <h1><b>mail 2</b></h1> <br />
+             <div className='flex p-2 m-2 bg-white w-full h-20 rounded-2xl'>
+                <h1><b>mail 3</b></h1> <br />
                 <h2>this is the mail</h2>
             </div>
-             <div className='flex p-2 m-2 bg-white w-full h-25 rounded-2xl'>
-                <h1><b>mail 2</b></h1> <br />
+             <div className='flex p-2 m-2 bg-white w-full h-20 rounded-2xl'>
+                <h1><b>mail 4</b></h1> <br />
                 <h2>this is the mail</h2>
-            </div> <div className='flex p-2 m-2 bg-white w-full h-25 rounded-2xl'>
-                <h1><b>mail 2</b></h1> <br />
+            </div> <div className='flex p-2 m-2 bg-white w-full h-20 rounded-2xl'>
+                <h1><b>mail 5</b></h1> <br />
                 <h2>this is the mail</h2>
             </div>
-             <div className='flex p-2 m-2 bg-white w-full h-25 rounded-2xl'>
-                <h1><b>mail 2</b></h1> <br />
+             <div className='flex p-2 m-2 bg-white w-full h-20 rounded-2xl'>
+                <h1><b>mail 6</b></h1> <br />
+                <h2>this is the mail</h2>
+            </div>
+            <div className='flex p-2 m-2 bg-white w-full h-20 rounded-2xl'>
+                <h1><b>mail 7</b></h1> <br />
+                <h2>this is the mail</h2>
+            </div>
+            <div className='flex p-2 m-2 bg-white w-full h-20 rounded-2xl'>
+                <h1><b>mail 8</b></h1> <br />
                 <h2>this is the mail</h2>
             </div>
         </div>
